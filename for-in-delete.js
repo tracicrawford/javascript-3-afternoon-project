1beedcd8a4a3ef0249a484612eb01fe10ffc5e47
Traci Crawford <tracicrawford@yahoo.com>
2019-11-15 15:47:40 -0600

    for-in-delete completed

--- for-in-delete.js
+++ for-in-delete.js
@@ -40,7 +40,11 @@
 */
 
 function showValues( obj ) {
-  //Code Here
+  let arr = [];
+  for (var key in obj) {
+    arr.push(obj[key])
+  }
+  return arr.join("");
 }
 
 
@@ -53,8 +57,14 @@ function showValues( obj ) {
   Return the updated object.
 */
 
-//Code Here
-
+function greaterThan10(obj) {
+  for(var key in obj) {
+    if(obj[key]>10){
+      obj[key]=0;
+    }
+  }
+  return obj;
+}
 
 
 ////////// PROBLEM 3 //////////
